e9670e6e220fb3e4ff2a07b14a022274c1351dd7
hansung1908 <hansung1908@naver.com>
2023-08-29 21:45:07 +0900

    자료형, 변수

---
+++ datatype.py
@@ -0,0 +1,21 @@
+# number
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+# string
+print('풍선')
+print("나비")
+print("ㅋ"*9)
+
+# boolean
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not True)
+print(not (5>10))
